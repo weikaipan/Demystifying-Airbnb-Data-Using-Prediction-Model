be463918ad901f55b0749c9cc76d674ff098f409
weikaipan <weikai.pan1023@gmail.com>
2018-06-03 02:24:18 -0400

    upload config

---
+++ src/config.py
@@ -0,0 +1,7 @@
+"""."""
+features = ["accommodates", "neighbourhood",
+            "bedrooms", "beds",
+            "bathrooms", "review_scores_rating",
+            "property_type", "room_type", "last_scraped"]
+path = '/home/wkp219/proj/data/'
+raw_path = '/home/wkp219/proj/data/raw/'

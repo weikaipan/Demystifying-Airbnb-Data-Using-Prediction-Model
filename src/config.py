3f268e2ee193bde9b7c6d3814139884b2b4368bf
weikaipan <weikai.pan1023@gmail.com>
2018-06-03 02:46:14 -0400

    debug

--- src/config.py
+++ src/config.py
@@ -3,6 +3,6 @@ features = ["accommodates", "neighbourhood",
             "bedrooms", "beds",
             "bathrooms", "review_scores_rating",
             "property_type", "room_type", "last_scraped"]
-label = "price"
+label = ["price"]
 path = '/home/wkp219/proj/data/'
 raw_path = '/home/wkp219/proj/data/raw/'
